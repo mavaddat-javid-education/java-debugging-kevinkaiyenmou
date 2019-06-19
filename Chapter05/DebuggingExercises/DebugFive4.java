e64e296a238f19ba4a2de69ddab68b531b62c510
kevin mou <kevinmou@kevins-Air.hitronhub.home>
2019-06-18 20:01:42 -0700

    finished debug excercises

--- Chapter05/DebuggingExercises/DebugFive4.java
+++ Chapter05/DebuggingExercises/DebugFive4.java
@@ -3,30 +3,35 @@
 import javax.swing.*;
 public class DebugFive4
 {
-   public static void main (String args[]) 
-   {
+  public static void main(String args[])
+    {
       int one, two, three, four;
-      String str, output;
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      one = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      four = Integer.parseInt();
-      if(one > two & one >= three && one < four)
-         output = "Highest is " + one;
+     String output;
+     //str = JOptionPane.showInputDialog(null,"Enter an integer");
+     one = getInt();
+     //str = JOptionPane.showInputDialog(null,"Enter an integer");
+     two = getInt();
+     //str = JOptionPane.showInputDialog(null,"Enter an integer");
+     three = getInt();
+     //str = JOptionPane.showInputDialog(null,"Enter an integer");
+     four = getInt();
+     if(one > two && one > three && one > four)
+        output = "Highest is " + one;
+     else
+        if(two > one && two > three && two > four)
+       output = "Highest is " + two;
+        else
+      if(three > one && three > two && three > four)
+        output = "Highest is " + three;
       else
-         if(two > one || two > three && two > four)
-            output = "Highest is " + two;
-         else
-           if(three > one && three > two || three == four)
-              output = "Highest is " + three;
-           else
-              output = "Highest is " + three;
-      JOptionPane.showMessageDialog(null, output);
+        output = "Highest is " + four;
+     JOptionPane.showMessageDialog(null, output);
+
+    }
+ public static int getInt()
+   {
+     String str = JOptionPane.showInputDialog(null,"Enter an integer");
+     int a = Integer.parseInt(str);
+     return a;
    }
 }
-
-
