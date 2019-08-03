aff8a5ff2c4687608d989ac88b59b2dacc4ac2f0
Kevin Mou <kevinmou@kevins-Air.hitronhub.home>
2019-08-02 18:07:04 -0700

    Corrected ch 6 debugging exercises up to #2

--- Chapter06/DebuggingExercises/DebugSix1.java
+++ Chapter06/DebuggingExercises/DebugSix1.java
@@ -1,19 +1,18 @@
 // DebugSix1.java
 // Start with a penny
 // double it every day
-   how much do you have in a 30-day month?
+//   how much do you have in a 30-day month?
 public class DebugSix1
 {
    public static void main(String args[])
    {
       final int DAYS = 30;
       double money = 0.01;
-      int day = 1;
-      while(day < days)
+      for(int i = 0; i < DAYS; i++)
       {
          money = 2 * money;
-         System.out.println("After day " + day +
-            " you have " + money);
+         System.out.println("After day " + String.format("%2d",i+1) +
+            " you have $" + String.format("%11.2f",money));
       }
    }
 }
--- Chapter06/DebuggingExercises/DebugSix2.java
+++ Chapter06/DebuggingExercises/DebugSix2.java
@@ -5,17 +5,17 @@ public class DebugSix2
 {
    public static void main(String args[])
    {
-      char letter;
       int a;
       final int MIN = 65;
-      final int MAX = 122;
-      final int STOPLINE1 = 85;
-      final int STOPLINE2 = 122;
-      for(a = MIN; a <= MAX; a++)
+      final int MAX = 65536;
+      char letter = (char)MIN;
+      for(a = MIN; a <= MAX; a++){
         letter = (char)a;
         System.out.print("  " + letter);
-        if((a == STOPLINE1) & (a == STOPLINE2))
+        if((a+1)%20 == 0)
           System.out.println();
-      System.out.println("\nEnd of application")
+      }
+
+      System.out.println("\nEnd of application");
     }
 }
