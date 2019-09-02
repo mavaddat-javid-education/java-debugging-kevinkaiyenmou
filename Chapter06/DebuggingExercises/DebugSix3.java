85bde24c96c6abf8da74eadc887b529dc679104c
Kevin Mou <kevinmou@kevins-Air.hitronhub.home>
2019-09-02 16:58:31 -0700

    Ignore unnecessary files

--- Chapter06/DebuggingExercises/DebugSix3.java
+++ Chapter06/DebuggingExercises/DebugSix3.java
@@ -6,35 +6,26 @@
 import javax.swing.*;
 public class DebugSix3
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws InterruptedException
   {
     String userNumString;
     int userNum, val;
     final int MIN = 1;
-    final int MAX = 20;
-    userNumString = JOptionPane.showInputDialog(null,
-        "Enter a number between  + MIN + " and " + MAX +  inclusive");
+    final int MAX = 200000000;
+    userNumString = JOptionPane.showInputDialog(null,"Enter a number between " + MIN + " and " + MAX + " inclusive");
     userNum = Integer.parseInt(userNumString);
     while(userNum < MIN || userNum > MAX)
     {
        userNumString = JOptionPane.showInputDialog(null,
-        "Number out of range" +
-        "\nEnter a number between " + MIN +  and " + MAX + " inclusive");
-       userNum = Integer.parseInt(userNuString);
+        "Number out of range" + "\nEnter a number between " + MIN + " and " + MAX + " inclusive");
+       userNum = Integer.parseInt(userNumString);
     }
-    for(val = userNum; val < 0; --val)
+    for(val = userNum; val > 0; --val)
     {
       System.out.print(val + "  ");
-      for(int x = 0; x < 100000; ++x)
-       for(int y = 0; y < 10000; ++y);
+      Thread.sleep(MAX/userNum);
        // Adjust these numbers for faster or slower performance
     }
     System.out.println("Blastoff!");
   }
 }
-
-
-
-
-
-
--- Chapter06/DebuggingExercises/DebugSix4.java
+++ Chapter06/DebuggingExercises/DebugSix4.java
@@ -1,40 +1,35 @@
 // DebugSix4.java
-// Displays 5 random numbers between 
+// Displays 5 random numbers between
 // (and including) user-specified values
 import java.util.Scanner;
 public class DebugSix4
 {
    public static void main(String[] args)
    {
-      int high, low, count;
+      int high, low, count=0;
       final int NUM = 5;
       Scanner input = new Scanner(System.in);
       // Prompt user to enter high and low values
-      System.out.print("This application displays " + NUM +
-         " random numbers" +
-         "\nbetween the low and high values you enter" +
-         "\nEnter low value now... ");
+      System.out.print("This application displays " + NUM +          " random numbers" +          "\nbetween the low and high values you enter" +          "\nEnter low value now... ");
       low = input.nextInt();
       System.out.print("Enter high value... ");
       high = input.nextInt();
-      while(low == high)
+      while(low <= high)
       {
-         System.out.println("The number you entered for a high number, " +
-            high + ", is not more than " + low);
+         System.out.println("The number you entered for a high number, " +             high + ", is not more than " + low);
          System.out.print("Enter a number higher than " + low + "... ");
          high = input.nextInt();
       }
-
-      while(count < low)
+      while(count < NUM)
       {
          double result = Math.random();
           // random() returns value between 0 and 1
          int answer = (int) (result * 10 + low);
           // multiply by 10 and add low -- random is at least the value of low
           // only use answer if it is low enough
-         if(answer <= low)
+         if(answer <= high || answer >= low)
          {
-            System.out.print(answer + "  ");
+            System.out.print(answer + " is the number the combuter chose ");
             ++count;
          }
       }
