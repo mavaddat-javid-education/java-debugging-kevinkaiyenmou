0f0bb470ae6e50476f99958eb5e660c73f475aa6
kevinkaiyenmou <51729662+kevinkaiyenmou@users.noreply.github.com>
2019-11-02 16:57:33 -0700

    First debug of ch 13
    Signed-off-by: kevinkaiyenmou <51729662+kevinkaiyenmou@users.noreply.github.com>

--- Chapter13/DebuggingExercises/DebugThirteen1.java
+++ Chapter13/DebuggingExercises/DebugThirteen1.java
@@ -7,32 +7,35 @@ public class DebugThirteen1
 {
    public static void main(String[] args)
    {
+      FileSystem fs = FileSystems.getDefault();
       Path file1 =
-         Path.get("C:\\Java\\Chapter.13\\DebugData1.txt");
+         fs.getPath("/Users/kevinmou/Documents/GitHub/java-debugging-kevinkaiyenmou/Chapter13/DebuggingExercises/DebugData1.txt");
       Path file2 =
-         Path.get("C:\\Java\\Chapter.13\\DebugData2.txt");
+         fs.getPath("/Users/kevinmou/Documents/GitHub/java-debugging-kevinkaiyenmou/Chapter13/DebuggingExercises/DebugData2.txt");
       try
       {
          BasicFileAttributes attr1 =
             Files.readAttributes(file1, BasicFileAttributes.class);
          System.out.println("File: " + file1.getFileName());
-         System.out.println("Creation time " + attr1.CreationTime());
-         System.out.println("Last modified time " + attr1.lastModifiedTime());
-         System.out.println("Size " + attr1.sze());
+         System.out.println("Creation time: " + attr1.creationTime());
+         System.out.println("Last modified time: " + attr1.lastModifiedTime());
+         System.out.println("Size: " + attr1.size());
          BasicFileAttributes attr2 =
             Files.readAttributes(file2, BasicFileAttributes.class);
          System.out.println("\nFile: " + file2.getFileName());
-         System.out.println("Creation time " + attr2.creationTime());
-         System.out.println("Last modified time " + attr2.lastModifiedTime());
-         System.out.println("Size " + attr2.size());
-         if(attr1.creationTime().compareTo(attr1.creationTime()) > 0)
-            System.out.println("\n" + file1getFileName() + " was created earlier");
+         System.out.println("Creation time: " + attr2.creationTime());
+         System.out.println("Last modified time: " + attr2.lastModifiedTime());
+         System.out.println("Size: " + attr2.size());
+         if(attr1.creationTime().compareTo(attr2.creationTime()) > 0)
+            System.out.println("\n" + file1.getFileName() + " was created after " + file2.getFileName());
          else
-            System.out.println("\n" + file1.getFileName() + " was not created earlier");
+            System.out.println("\n" + file1.getFileName() + " was not created after " + file2.getFileName());
          if(attr1.size() > attr2.size())
-            System.out.println(file1.getFileName() + " is larger ");
+            System.out.println(file1.getFileName() + " is larger than " + file2.getFileName());
+         else if(attr1.size() < attr2.size())
+            System.out.println(file1.getFileName() + " is smaller than " + file2.getFileName());
          else
-            System.out.println(file3.getFileName() + " is not larger");
+            System.out.println(file1.getFileName() + " is the same size as " + file2.getFileName());
       }
       catch(IOException e)
       {
