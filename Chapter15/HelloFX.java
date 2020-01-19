e2bb2cd51ef1b86635bcd65635b194829e1713fe
kevinkaiyenmou <51729662+kevinkaiyenmou@users.noreply.github.com>
2020-01-18 17:40:07 -0800

    updated HelloFX to work

--- Chapter15/HelloFX.java
+++ Chapter15/HelloFX.java
@@ -3,7 +3,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+/**  To compile, make sure to set the --module-path and the -cp in `javac`. So you will run something like
+ * javac --module-path "$(eval echo ~$USER)/Downloads/javafx-sdk-14/lib/*" -cp "$(eval echo ~$USER)/Downloads/javafx-sdk-14/lib/*" -d . HelloFX.java
+   java --module-path "$(eval echo ~$USER)/Downloads/javafx-sdk-14/lib"  --add-modules javafx.controls,javafx.fxml HelloFX 
+ */
 public class HelloFX extends Application {
 
     @Override
